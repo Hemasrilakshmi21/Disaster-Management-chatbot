00efafae008798e7ab2866253e1cc72f6655f7cf
P.Hema Sri Lakshmi <hemasrilakshmi21@gmail.com>
2026-06-05 14:07:48 +0530

    Update API key to placeholder
    Replaced the API key with a placeholder for security.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from google import genai
-API_KEY = "AIzaSyCC8b2bCSdEVZYbBpPGIUqcnuJqU2lrjk4" 
+API_KEY = "your api key" 
 client = genai.Client(api_key=API_KEY)
 st.set_page_config(
     page_title="Disaster Management Explainer Bot",
